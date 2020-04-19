355c70d02cee965bcce88f725885b4ed2099f4ce
Tom <tom@tom-laptop.home>
2020-04-19 13:00:31 +0100

    first commit, html ready

---
+++ index.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Help Landlords</title>
+    <link rel="stylesheet" href="styles/styles.css" />
+</head>
+<body>
+
+    <header>
+        <div class="container header">
+            <div class="header-logo">
+                <h3>HelpingLandlords</h3>
+            </div>
+
+            <div class="header-time">
+                <p>Monday - Friday : 9:00 am - 5:30 pm</p>
+                <p class="whatsapp"><img src="./img/whatsapp.svg" alt="whatsapp">+44 (0) 776 3380 426</p>
+            </div>
+        </div>
+    </header>
+
+    <section class="banner">
+        <div class="container">
+            <div class="banner-left">
+                <h3>Covid-19 Update</h3>
+                <h3>Rent Guaranteed For <br>
+                    Landlords in London</h3>
+                <p>We guaranty your rent even in time of crisis. No Voids, no risk. Your rent is paid into your bank every month !</p>
+                <a href="#contactUs">Contact Us Today</a>
+            </div>
+        </div>
+
+        <img src="./img/london.jpg" alt="">
+    </section>
+
+    <section class="why-us">
+        <div>
+            <h3>Why Choose Us ?</h3>
+        </div>
+
+        <div class="why-card--container">
+            <div class="why-card">
+                <span class="orange-box"></span>
+                <h3>Free of charge</h3>
+                <p>At HelpingLandlords, there are no fees or bolt on hidden charges – EVER!</p>
+            </div>
+
+            <div class="why-card">
+                <span class="orange-box"></span>
+                <h3>We find the tenants</h3>
+                <p>We take care of finding residents for your property. We handle all of the viewings.</p>
+            </div>
+
+            <div class="why-card">
+                <span class="orange-box"></span>
+                <h3>We Guarantee Rent</h3>
+                <p>We keep things simple for you. Your rent is paid into your bank every month.</p>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="reviews">
+        <div class="container">
+            <h3>Our Clients’ reviews</h3>
+            <div class="reviews-card--container">
+                <div class="review-card">
+                    <img src="" alt="">
+                    <p>HelpingLandlords have helped me a lot! They take care of everything and pay me a fixed long term rent every month without fail. I am delighted that I have saved time and money by working with them!</p>
+                    <p class="review-name">Anthony, London</p>
+                </div>
+
+                <div class="review-card">
+                    <img src="" alt="">
+                    <p>Since the Covid outbreak, I have had real challenges keeping all of my properties filled 100% of the time. Since working with HelpingLandlords, I have found a way of avoiding rent loss !</p>
+                    <p class="review-name">Emma, London</p>
+                </div>
+
+                <div class="review-card">
+                    <img src="" alt="">
+                    <p>HelpingLandlords saved me from repossession! They payed me a fixed long term rent every month that really helped me get my finances back!</p>
+                    <p class="review-name">Emma, London</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="contact" id="contact">
+        <h3>Contact Us Today !</h3>
+        <form name="bottomOfPage" class="contact-form" method="post">
+            <input placeholder="First & Last Name" type="text" name="name" id="name" maxlength="255">
+            <input placeholder="Email" type="email" name="email" id="email" maxlength="255">
+            <input placeholder="Phone number" type="text" name="subject" id="subject" maxlength="255">
+            <textarea cols="30" class="contact-form--input-mesage" rows="8" name="query" id="query" placeholder="Tell me more about your project"></textarea><br>
+            <input class="bottom-contact-form--submit" type="submit" value="SEND MESSAGE">
+        </form>
+    </section>
+
+    <footer></footer>
+
+</body>
+</html>
